84c083b50cf0d50257b87a5e94f3d5239bfb74ee
AshishPine22 <115570706+AshishPine22@users.noreply.github.com>
2023-02-11 22:36:47 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main()
+ {
+    FILE *fp;
+
+    fp = fopen("good.txt", "w");
+
+    fprintf(fp, "This is a string.\nAnother line with the string.\nJust a simple line.");
+    
+    fclose(fp);
+    return 0;
+}
